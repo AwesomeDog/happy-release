d88942383f840e1517fecdfc35bbe119a534be59
AwesomeDog <107466966+AwesomeDog@users.noreply.github.com>
2023-03-25 18:25:09 +0800

    0.0.2

--- public/prds/ir/invest-research.user.js
+++ public/prds/ir/invest-research.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         invest-research
 // @namespace    http://tampermonkey.net/
-// @version      0.0.1
+// @version      0.0.2
 // @description  try to take over the world!
 // @author
 // @match             *://*.10jqka.com.cn/*
@@ -19,6 +19,6 @@
   console.log("Starting 👌👌👌👌👌 invest-research. chrome://flags/Block insecure private network requests")
   if (location.href === 'http://localhost:8080/') return
   var script = document.createElement('script')
-  script.src = 'https://github.com/AwesomeDog/happy-release/releases/download/ir0.0.1/app.bundle.js'
+  script.src = 'https://github.com/AwesomeDog/happy-release/releases/download/ir0.0.2/app.bundle.js'
   document.body.appendChild(script)
 })()
